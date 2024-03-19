f3112c72620077ee5ede6b527c4f3a71431a6482
g4share <g4share@gmail.com>
2024-03-19 14:53:19 +0200

    Pipeline Demo - Database

---
+++ README.md
@@ -0,0 +1,11 @@
+## Pipeline Demo - Database
+
+### Start Postgres
+```console
+docker compose up
+```
+
+### Flyway
+```console
+./mvnw flyway:migrate
+```
---
+++ src/main/resources/db/migration/V1__create_users_table.sql
@@ -0,0 +1,9 @@
+-- CREATE USER TABLE
+CREATE TABLE users
+(
+    id BIGSERIAL NOT NULL,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) NOT NULL
+);
+ALTER TABLE users ADD CONSTRAINT pk_user_id PRIMARY KEY (id);
